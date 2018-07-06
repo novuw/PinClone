cc54e168b8dd458f2c7a31beb8859e60d8a51c95
Glitch (pinclone) <none>
2018-07-06 03:08:31 +0000

    📙👻 Updated with Glitch

--- server.js
+++ server.js
@@ -5,35 +5,35 @@ var request = require('superagent');
 var mongo = require('mongodb').MongoClient;
 var path = require('path');
 var passport = require('passport');
-var GitHubStrategy = require('passport-github').Strategy;
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
 //https://www.youtube.com/watch?v=kDhYUPcDS28&list=PL4cUxeGkcC9jdm7QX143aMLAqyM-jTZ2x&index=5
 
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.static('public'));
-/*
-passport.use(new GitHubStrategy({
+passport.use(new GoogleStrategy({
   //options
   clientID: process.env.clientid,
   clientSecret: process.env.secret,
   callbackURL: "https://pinclone.glitch.me/user/signin/callback"
-}), ()=> {
+}, ()=> {
   //passport callback function
 
-});
-*/
+}));
+app.use('/user/signin/callback', passport.authenticate('google', {
+  scope:['profile']
+}));
+
+
+
+
+
+
+
 app.get('/', function(request, response) {
   response.render('index');
 });
-app.use('/user/signin/callback', function(req, res){
-  res.send('logging in');
-});
-
-
-
-
-
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
